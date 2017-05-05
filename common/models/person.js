d99e6be421336f7d66d88e4a805abe3a15c1ee70
cesardesarrollo <cesar_alonso_m_g@hotmail.com>
2017-05-05 15:56:44 -0500

    midlewares y hooks

--- common/models/person.js
+++ common/models/person.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(Person) {
-
+    Person.beforeRemote('*.__create__messages', function(ctx, instance, next) {
+       var body = ctx.args.data;
+       var now = new Date();
+       var now_utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+       body.created = now_utc;
+       console.log(body)
+       next(); 
+    });
 };
