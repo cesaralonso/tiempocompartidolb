2913e97d3dcd3084e3f9bbd7e1483c347bf722a6
cesardesarrollo <cesar_alonso_m_g@hotmail.com>
2017-06-07 22:44:29 -0500

    https

---
+++ server/private/ssl-config.js
@@ -0,0 +1,9 @@
+var crypto = require('crypto');
+var fs = require("fs");
+var path = require('path');
+
+exports.privateKey = fs.readFileSync(path.join(__dirname, 'key.pem')).toString();
+exports.certificate = fs.readFileSync(path.join(__dirname, 'cert.pem')).toString();
+exports.credentials = {key: exports.privateKey, cert: exports.certificate};
+
+// $ openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
