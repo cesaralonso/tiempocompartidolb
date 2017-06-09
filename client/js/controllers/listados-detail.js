8ba8b0d238a0c0b1629753643451844b1764e8d9
cesardesarrollo <cesar_alonso_m_g@hotmail.com>
2017-06-09 17:07:52 -0500

    listados de clubes, con padding y membresia

--- client/js/controllers/listados-detail.js
+++ client/js/controllers/listados-detail.js
@@ -9,55 +9,72 @@ angular
 
     $scope.listados = [];
 
-    var categoria = $stateParams.categoria;
-    var subcategoria = $stateParams.subcategoria;
-
-
-    var club_destino = $stateParams.titulo.replace('Rentas-Vacacionales-En-', '').split('--');
-    var club = club_destino[0].replace(new RegExp('-', 'g'), ' ');
-    var destino = club_destino[1].replace(new RegExp('-', 'g'), ' ');
-    var titulo = $stateParams.titulo.replace(new RegExp('-', 'g'), ' ');
-
-    $scope.destino = destino;
-    $scope.club = club;
-    $scope.titulo = titulo;
-
-    console.log("titulo",titulo);
-    console.log("destino",destino);
-    console.log("club",club);
-
-    var where_clubes = {
-        and: [{
-            clubNombre: club
-        },{
-            localidadNombre: destino
-        }]
-    };
-
-    var where_paises = {
-        paisNombre: pais
-    };
-
-    var where_destinos = {
-        localidadNombre: destino
-    };
-    var where_unidades = {
-        localidadNombre: unidad
-    };
-
-    var where_amenidades = {
-        localidadNombre: amenidad
-    };
-
-
 
     var where = '';
     if(categoria === 'clubes') {
-        where = where_clubes;
+        var where = {
+            and: [{
+                clubNombre: club
+            },{
+                localidadNombre: destino
+            }]
+        };
+
+        var categoria = $stateParams.categoria;
+        var subcategoria = $stateParams.subcategoria;
+
+        var club_destino = $stateParams.titulo.replace('Rentas-Vacacionales-En-', '').split('--');
+        var club = club_destino[0].replace(new RegExp('-', 'g'), ' ');
+        var destino = club_destino[1].replace(new RegExp('-', 'g'), ' ');
+        var titulo = $stateParams.titulo.replace(new RegExp('-', 'g'), ' ');
+
+        $scope.destino = destino;
+        $scope.club = club;
+        $scope.titulo = titulo;
+
+        console.log("titulo",titulo);
+        console.log("destino",destino);
+        console.log("club",club);
+
+
     } else if(categoria === 'paises') {
-        where = where_paises;
+        var where = {
+            localidadNombre: destino
+        };
+        var categoria = $stateParams.categoria;
+        var subcategoria = $stateParams.subcategoria;
+
+        var club_destino = $stateParams.titulo.replace('Rentas-Vacacionales-En-', '').split('--');
+        var club = club_destino[0].replace(new RegExp('-', 'g'), ' ');
+        var destino = club_destino[1].replace(new RegExp('-', 'g'), ' ');
+        var titulo = $stateParams.titulo.replace(new RegExp('-', 'g'), ' ');
+
+        $scope.destino = destino;
+        $scope.club = club;
+        $scope.titulo = titulo;
+
+        console.log("titulo",titulo);
+        console.log("destino",destino);
+        console.log("club",club);
+
     } else if(categoria === 'destinos') {
-        where = where_destinos;
+        where = {};
+        var categoria = $stateParams.categoria;
+        var subcategoria = $stateParams.subcategoria;
+
+        var club_destino = $stateParams.titulo.replace('Rentas-Vacacionales-En-', '').split('--');
+        var club = club_destino[0].replace(new RegExp('-', 'g'), ' ');
+        var destino = club_destino[1].replace(new RegExp('-', 'g'), ' ');
+        var titulo = $stateParams.titulo.replace(new RegExp('-', 'g'), ' ');
+
+        $scope.destino = destino;
+        $scope.club = club;
+        $scope.titulo = titulo;
+
+        console.log("titulo",titulo);
+        console.log("destino",destino);
+        console.log("club",club);
+
     }
     
     console.log("where",where);
