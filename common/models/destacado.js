d0b8292199fed8f18d585360b343830596f3156c
cesardesarrollo <cesar_alonso_m_g@hotmail.com>
2017-08-25 10:48:25 -0500

    membresia belogsTo in destacados

--- common/models/destacado.js
+++ common/models/destacado.js
@@ -1,5 +1,21 @@
 'use strict';
 
+var _ = require('lodash');
+
 module.exports = function(Destacado) {
+    
+    Destacado.beforeRemote('find', function(ctx, instance, next) {
+        console.log('Entró a destacado get membresias');
+        if (!ctx.args.filter) {
+            ctx.args.filter = {};
+        }
+        ctx.args.filter.include = [ "membresia"];
+        if (!ctx.args.filter.where) {
+            ctx.args.filter.where = {};
+        }
+        ctx.args.filter.where = _.merge(ctx.args.filter.where, {});
+        ctx.args.filter.order = "entrada DESC"
+        next();
+    });
 
 };
